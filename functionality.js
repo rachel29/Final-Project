15bbfb5d66e9acedfcc31895a3492801d6d6b023
rachel29 <rachelliu29@gmail.com>
2017-08-01 11:48:39 -0700

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -2,18 +2,3 @@
 display: inline />
 <input type="button" onclick="location.href='signup.html';" value="Sign Up"display: inline />
 
-
-function previewFile() {
-  var preview = document.querySelector('img');
-  var file    = document.querySelector('input[type=file]').files[0];
-  var reader  = new FileReader();
-
-  reader.addEventListener("load", function () {
-    preview.src = reader.result;
-  }, false);
-
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-}
-window.previewFile = previewFile;
