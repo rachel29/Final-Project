067052ffaee5a4c9189fcb571f3dafb4a0640915
rachel29 <rachelliu29@gmail.com>
2017-08-01 11:41:15 -0700

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,7 +1,7 @@
 <input type="button" onclick="location.href='login.html';" value="Login" 
 display: inline />
 <input type="button" onclick="location.href='signup.html';" value="Sign Up"display: inline />
-.uploadphoto {
+
 
 function previewFile() {
   var preview = document.querySelector('img');
