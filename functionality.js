e6dfb967bb09a3bc96aefea8b0e345c64fa56cb3
rachel29 <rachelliu29@gmail.com>
2017-08-03 09:59:30 -0700

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,4 +1,5 @@
 <input type="button" onclick="location.href='login.html';" value="Login" 
 display: inline />
 <input type="button" onclick="location.href='signup.html';" value="Sign Up"display: inline />
+<input type="button" onclick="location.href='textbooksbrowse.html';" value="Browse"display: inline />
 
